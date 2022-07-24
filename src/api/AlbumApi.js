a641a85e2eb2caaa7b7cd03f76274414794b1c60
huy27 <huy27297@gmail.com>
2022-07-24 12:49:07 +0700

    Update album name

--- src/api/AlbumApi.js
+++ src/api/AlbumApi.js
@@ -5,3 +5,8 @@ export const getAlbumsApi = async () => {
     const result = await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/Album`);
     return result.data;
 }
+
+export const getAlbumByIdApi = async (id) => {
+    const result = await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/Album/${id}`);
+    return result.data;
+}
--- src/components/Album.js
+++ src/components/Album.js
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 // import DataSongs from "./../data/songs.json";
 import { useLocation, useParams } from "react-router-dom";
+import { getAlbumByIdApi } from "../api/AlbumApi";
 import { ShowLyric } from "../reducer/LyricSlice";
 import { SetSongs } from "../reducer/SongsSlice";
 import { getSongsByAlbumIdApi } from "./../api/SongApi";
@@ -11,43 +12,47 @@ import Navbar from "./Navbar";
 import Playing from "./Playing";
 
 const Album = () => {
-  const dispatch = useDispatch();
-  const location = useLocation();
-  const { id } = useParams();
-  const { albumName, searchSongs } = location.state;
-  const isShowLyric = useSelector((state) => state.Lyric.isShow);
+    const dispatch = useDispatch();
+    const location = useLocation();
+    const { id } = useParams();
+    const [ albumName, setAlbumName ] = useState("");
+    const isShowLyric = useSelector((state) => state.Lyric.isShow);
 
-  const getSongs = async () => {
-    let Songs = [];
-    if (id === "favorite") {
-      Songs = JSON.parse(localStorage.getItem("Favorite"));
-    } else if (!id && searchSongs.length !== 0) {
-      Songs = [...searchSongs];
-    } else {
-      Songs = await getSongsByAlbumIdApi(id);
-    }
-    if (Songs) {
-      dispatch(SetSongs(Songs));
-      dispatch(ShowLyric({ isShow: false, lyric: Songs[0]?.lyric }));
-    }
-  };
+    const getSongs = async () => {
+        let Songs = [];
+        if (id === "favorite") {
+            Songs = JSON.parse(localStorage.getItem("Favorite"));
+            setAlbumName("Sở thích");
+        } else if (!id && location) {
+            Songs = [...location.state.searchSongs];
+            setAlbumName("SearchSongs");
+        } else {
+            Songs = await getSongsByAlbumIdApi(id);
+            const album = await getAlbumByIdApi(id);
+            setAlbumName(album.name);
+        }
+        if (Songs) {
+            dispatch(SetSongs(Songs));
+            dispatch(ShowLyric({ isShow: false, lyric: Songs[0]?.lyric }));
+        }
+    };
 
-  useEffect(() => {
-    getSongs();
-  }, []);
+    useEffect(() => {
+        getSongs();
+    }, []);
 
-  return (
-    <>
-      <div className="App">
-        <Navbar albumName={albumName} />
-        <div className="grid grid-cols-3 bg-slate-700 h-screen-navbar-player overflow-hidden">
-          <ListSongs height={isShowLyric ? "calc(100vh - 6rem - 20rem)" : ""} />
-          {isShowLyric && <Lyric />}
-        </div>
-        <Playing />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="App">
+                <Navbar albumName={albumName} />
+                <div className="grid grid-cols-3 bg-slate-700 h-screen-navbar-player overflow-hidden">
+                    <ListSongs height={isShowLyric ? "calc(100vh - 6rem - 20rem)" : ""} />
+                    {isShowLyric && <Lyric />}
+                </div>
+                <Playing />
+            </div>
+        </>
+    );
 };
 
 export default Album;
